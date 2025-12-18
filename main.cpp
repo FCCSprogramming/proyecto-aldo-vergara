a1b947b729378e39a9870bee833fc02a7619465d
Aldo-26 <aldo.vergara.a@uni.pe>
2025-12-18 06:38:03 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,27 @@
+#ifndef ESTUDIANTE_H
+#define ESTUDIANTE_H
 
+#include "Persona.h"
+
+struct RegistroNotas {
+    float* notas;     
+    int cantidad;      
+    int capacidad;     
+};
+
+class Estudiante : public Persona {
+private:
+    RegistroNotas* historial; 
+public:
+    Estudiante(int id, std::string nombre, std::string apellido);
+    
+    ~Estudiante() override;                  
+    Estudiante(const Estudiante& other);     
+    
+    void agregarNota(float nota);
+    float calcularPromedio() const;
+
+    void mostrarInfo() const override;
+};
+
+#endif
