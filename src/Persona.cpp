7017c1fd2dfb1fa9b5c9539b5ee0fda640b8e653
Aldo-26 <aldo.vergara.a@uni.pe>
2025-12-19 10:02:44 -0500

    Create Persona.cpp

---
+++ src/Persona.cpp
@@ -0,0 +1,6 @@
+#include "../include/Persona.h"
+
+Persona::Persona(int id, std::string nombre, std::string apellido) 
+    : id(id), nombre(nombre), apellido(apellido) {
+    // Inicialización simple
+}
